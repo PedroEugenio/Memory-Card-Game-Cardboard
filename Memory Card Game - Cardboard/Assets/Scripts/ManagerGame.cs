097cbfd4d3c50f7a2b4c3799d03688ba4af20a0d
Pedro Eugénio <p.geno_88@hotmail.com>
2018-03-13 18:49:39 +0000

    Static GameObjects

--- Memory Card Game - Cardboard/Assets/Scripts/ManagerGame.cs
+++ Memory Card Game - Cardboard/Assets/Scripts/ManagerGame.cs
@@ -30,11 +30,12 @@ public class ManagerGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(GvrPointerInputModule.CurrentRaycastResult.gameObject.tag == "Back" && GvrPointerInputModule.Pointer.TriggerDown && canFlip)
-        {
-            addCard(GvrPointerInputModule.CurrentRaycastResult.gameObject.transform.parent.gameObject); //Gets the parent of the hitted gameobject (back)
-            Debug.Log(GvrPointerInputModule.CurrentRaycastResult.gameObject.name);
-        }
+        if(GvrPointerInputModule.CurrentRaycastResult.gameObject != null)
+            if(GvrPointerInputModule.CurrentRaycastResult.gameObject.tag == "Back" && GvrPointerInputModule.Pointer.TriggerDown && canFlip)
+            {
+                addCard(GvrPointerInputModule.CurrentRaycastResult.gameObject.transform.parent.gameObject); //Gets the parent of the hitted gameobject (back)
+                Debug.Log(GvrPointerInputModule.CurrentRaycastResult.gameObject.name);
+            }
         
 	}
 
